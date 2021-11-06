00398d2de73ed6bdec0a8d2dfa4c700efbe132cd
Andrew <8911102@gmail.com>
2021-11-06 22:44:26 +0500

    function addMarkup was moved to separated module

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,5 @@
-// Get the modal
+import addMarkup from "./add-markup.js";
+
 const dealings = [
   {
     id: 260,
@@ -100,88 +101,6 @@ window.onclick = function (event) {
   }
 }
 
-const addMarkup = (data) => {
-  const obj = JSON.parse(data);
-
-  const formElement = document.querySelector('#modal-form');
-
-  const list = document.createElement('ul');
-  list.className = 'list';
-  const template = document.querySelector(".template");
-
-  obj.forEach((item) => {
-    const listItem = template.content.cloneNode(true);
-    const paragraph = listItem.querySelector(".deal__name");
-    paragraph.innerHTML = item.name;
-
-    const radioButtonOpen = listItem.querySelector("#radio-open");
-    radioButtonOpen.name += '-' + item.id;
-    radioButtonOpen.id += '-' + item.id;
-
-    const labelOpen = listItem.querySelector(".deal__label--open");
-    labelOpen.htmlFor += '-' + item.id;
-
-    const radioButtonSuccess = listItem.querySelector("#radio-success");
-    radioButtonSuccess.name += '-' + item.id;
-    radioButtonSuccess.id += '-' + item.id;
-
-    const labelSuccess = listItem.querySelector(".deal__label--success");
-    labelSuccess.htmlFor += '-' + item.id;
-
-    const radioButtonFail = listItem.querySelector("#radio-fail");
-    radioButtonFail.name += '-' + item.id;
-    radioButtonFail.id += '-' + item.id;
-
-    const labelFail = listItem.querySelector(".deal__label--fail");
-    labelFail.htmlFor += '-' + item.id;
-
-    switch (item.kind) {
-      case 'open':
-        radioButtonOpen.checked = true;
-        break;
-      case 'success':
-        radioButtonSuccess.checked = true;
-        break;
-      case 'fail':
-        radioButtonFail.checked = true;
-        break;
-    }
-
-    list.appendChild(listItem);
-  });
-
-  formElement.appendChild(list);
-
-  const buttonSubmit = document.createElement('button');
-  buttonSubmit.textContent = 'Сохранить';
-  buttonSubmit.className = 'modal__submit';
-  buttonSubmit.type = 'submit';
-
-  const getDataList = (data) => {
-    const list = [];
-
-    for (const [name, status] of Object.entries(data)) {
-      const id = name.split('-')[1];
-      list.push({id: id , kind: status});
-    }
-
-    return list;
-  };
-
-  formElement.onsubmit = (evt) => {
-    evt.preventDefault();
-    const formData = new FormData(formElement);
-    const data = Object.fromEntries(formData.entries());
-    alert(JSON.stringify(getDataList(data)));
-  };
-
-  buttonSubmit.onclick = () => {
-    modal.style.display = "none";
-  };
-
-  formElement.appendChild(buttonSubmit);
-};
-
 document.addEventListener('keydown',
   (event) => {
     if (event.key === 'Escape') {
@@ -189,7 +108,7 @@ document.addEventListener('keydown',
     }
 })
 
-addMarkup(dataJSON);
+addMarkup(dataJSON, modal);
 
 btn.onclick = function () {
   modal.style.display = "block";
